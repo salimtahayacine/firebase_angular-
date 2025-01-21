8ec63382a5c1dd024624d5d6b8c72cd499018379
Salim Taha Yacine <97783478+salimtahayacine@users.noreply.github.com>
2025-01-21 16:53:18 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyCQt3Lf-9SWLtCG9jS72ySI9-1CQIX8E0g",
-    authDomain: "angular-notificationpsh.firebaseapp.com",
-    projectId: "angular-notificationpsh",
-    storageBucket: "angular-notificationpsh.firebasestorage.app",
-    messagingSenderId: "953681304280",
-    appId: "1:953681304280:web:896b00554d3623ae1455ad",
-    vapidKey: "BNorIQzlw2WV-y9E6lEJMMnl7K_tv6Ajj-IErgb8Th5nFKh7iQ5Ghl-WWmWNAfm15bGZbGXeTeWsBVjFlW7z92Y"
+    apiKey: "your-api-key",
+    authDomain: "your-project-id.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project-id.appspot.com",
+    messagingSenderId: "your-messaging-sender-id",
+    appId: "your-app-id",
+    vapidKey: ""
   }
 };
